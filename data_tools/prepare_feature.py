43f6a39768c9bf19da12ce96bd333f9a8653edcd
rehohoho <ernie.birdy.ho@gmail.com>
2022-02-06 10:53:40 +0800

    Add script to generate features.

---
+++ data_tools/prepare_feature.py
@@ -0,0 +1,72 @@
+import argparse
+import glob
+import os
+
+import pandas as pd
+from scipy.interpolate import griddata, RegularGridInterpolator
+import numpy as np
+import matplotlib.pyplot as plt
+
+CHUNKS = 1000
+ICHUNKS = 1000j
+
+
+def get_interpolate_fn(geodf: pd.DataFrame,
+                       geofeature: pd.DataFrame,
+                       feature_name: str):
+
+    minlng, maxlng = geodf['rawlng'].min(), geodf['rawlng'].max()
+    minlat, maxlat = geodf['rawlat'].min(), geodf['rawlat'].max()
+    print('lats limits', minlng, maxlng)
+    print('lngs limits', minlat, maxlat)
+    grid_x, grid_y = np.mgrid[minlng:maxlng:ICHUNKS, minlat:maxlat:ICHUNKS]
+    grid = griddata(geofeature[['longitude', 'latitude']], 
+                    geofeature[[feature_name]], 
+                    (grid_x, grid_y), method='linear')
+    
+    x_idx = np.linspace(minlng, maxlng, num=CHUNKS)
+    y_idx = np.linspace(minlat, maxlat, num=CHUNKS)
+    f = RegularGridInterpolator((x_idx, y_idx), grid, method='linear')
+    
+    # np.savetxt('grid.csv', grid.squeeze())
+    return grid, f
+
+
+def generate_plot(grid,
+                  geodf: pd.DataFrame,
+                  out_path: str):
+    minlng, maxlng = geodf['rawlng'].min(), geodf['rawlng'].max()
+    minlat, maxlat = geodf['rawlat'].min(), geodf['rawlat'].max()
+    plt.figure(figsize=(10,10))
+    plt.imshow(grid, extent=(minlng, maxlng, minlat, maxlat))
+    plt.savefig(out_path)
+
+
+def get_feature(geodf: pd.DataFrame,
+                f):
+    
+    def interpolate_feature(x, f):
+        return f((x[1], x[0]))[0]
+    
+    return df[['rawlat', 'rawlng']].apply(interpolate_feature, args=(f,), axis=1)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('--df_folder', type=str)
+    parser.add_argument('--feature_csv', type=str)
+    args = parser.parse_args()
+
+    feature_df = pd.read_csv(args.feature_csv)
+    df_paths = [i for i in glob.glob(
+        os.path.join(args.df_folder, "*.parquet"), recursive=True)]
+    
+    for i, df_path in enumerate(df_paths):
+        df = pd.read_parquet(df_path)
+        grid, f = get_interpolate_fn(df, feature_df, feature_name='population_2020')
+        generate_plot(grid, df, out_path=f'heatmap{i}.png')
+
+        new_feature = get_feature(df, f)
+        df['population_2020'] = new_feature
+        df.to_parquet(f'new_df{i}.parquet')
